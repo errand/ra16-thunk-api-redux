2d28153111778cf3e8aad06a15b9cb0186cb7259
Aleksandr Shatskikh <work@errand.ru>
2022-05-17 22:16:27 +0300

    feat: adding state for the form

--- src/components/TaskEditForm.jsx
+++ src/components/TaskEditForm.jsx
@@ -1,6 +1,6 @@
 import {fetchServiceById} from "../thunks/serviceThunk";
 import {useDispatch, useSelector} from "react-redux";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import { useParams} from "react-router-dom";
 
 export default function TaskEditForm() {
@@ -12,17 +12,32 @@ export default function TaskEditForm() {
   const loading = useSelector(state => state.tasks.loading)
   const error = useSelector(state => state.tasks.error)
 
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+  const [content, setContent] = useState('')
+
+  const handleSubmit = () => {
+
+  }
+
   useEffect(() => {
     dispatch(fetchServiceById(id))
   },[dispatch])
 
   return (
     <>
-    {loading === 'pending' ? <div className="spinner"></div> :
-      <form>
-        <input type="text" value={task.name}/>
-        <input type="text" value={task.price}/>
-        <input type="text" value={task.content}/>
+      {error && <div className="alert alert-danger">{error}</div>}
+      {loading === 'pending' ? <div className="spinner"></div> :
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <input onChange={(e) => setName(e.target.value)} type="text" placeholder="name" value={task.name} className="form-control" />
+        </div>
+        <div className="mb-3">
+          <input onChange={(e) => setPrice(e.target.value)} placeholder="price" type="text" value={task.price} className="form-control" />
+        </div>
+        <div className="mb-3">
+          <input onChange={(e) => setContent(e.target.value)} placeholder="content" type="text" value={task.content} className="form-control" />
+        </div>
       </form> }
     </>
     )
